50747c8360a61538d34f9b571b52519bf49c1896
VladMaska <k.vlad2002@gmail.com>
2017-09-04 20:57:39 +0300

    + пробели

--- src/OffLineZad2.java
+++ src/OffLineZad2.java
@@ -1,9 +1,9 @@
 public class OffLineZad2 {
     public static void main(String[] args) {
         System.out.println("        /\\");
-        System.out.println("      / || \\");
-        System.out.println("     /  ||  \\");
-        System.out.println("    /   ||   \\");
+        System.out.println("      /   || \\");
+        System.out.println("     /    ||  \\");
+        System.out.println("    /   | |   \\");
         System.out.println("   /       ||    \\");
         System.out.println("  /        ||     \\");
         System.out.println("        || ");
