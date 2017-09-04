c521bb9c5baa8d67b3c0e09680e90aee55bdd7f7
sashaporitskiy <31014377+sashaporitskiy@users.noreply.github.com>
2017-09-04 20:06:00 +0300

    проба2

--- src/OffLineZad2.java
+++ src/OffLineZad2.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class OffLineZad2 {
+    public static void main(String[] args) {
+        System.out.println("        /\\");
+        System.out.println("      / || \\");
+        System.out.println("     /  ||  \\");
+        System.out.println("    /   ||   \\");
+        System.out.println("   /    ||    \\");
+        System.out.println("  /     ||     \\");
+        System.out.println("        || ");
+        System.out.println(" ________________");
+        System.out.println(" ________________");
+    }
 }
